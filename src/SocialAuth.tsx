1214260c9611aa56106ed594779df4144a78aea4
William Connelly <connelly.william@gmail.com>
2021-04-08 10:59:30 +0800

    feat(js): sign out
    - js bridge interface

--- src/SocialAuth.tsx
+++ src/SocialAuth.tsx
@@ -1,4 +1,5 @@
 import { NativeModules, Platform } from "react-native"
+import { SocialAuthError, SocialAuthErrorCode } from "social-auth"
 import { Config } from "./config"
 import type { IdentityProvider, SocialAuthResponse } from "./SocialAuthResponse"
 
@@ -24,19 +25,68 @@ const { SocialAuth } = NativeModules
 
 const Caller = SocialAuth as SocialAuthType
 
+export function isProviderInvalid(provider: string): boolean {
+
+    switch (provider) {
+
+        case "google":
+            if (Config.googleClientID === undefined || Config.googleClientID.length == 0) {
+                console.warn(SocialAuthError.withCode(SocialAuthErrorCode.CLIENT_ID))
+                return true
+            } else {
+                return false
+            }
+
+        case "apple":
+            if (Platform.OS === "android") {
+                console.error(SocialAuthError.withCode(SocialAuthErrorCode.IOS_ONLY))
+                return true
+            } else {
+                return false
+            }
+
+        case "facebook":
+            if (Config.facebookAppID === undefined || Config.facebookAppID.length == 0) {
+                console.warn(SocialAuthError.withCode(SocialAuthErrorCode.CLIENT_ID))
+                return true
+            } else {
+                return false
+            }
+    }
+
+    return true
+}
+
+
+export async function signOut(token: string, provider?: string): Promise<boolean> {
+    var success = false
+
+    if (provider) {
+        if (isProviderInvalid(provider)) {
+            return false
+        }
+
+        success = await Caller.signOut(token, provider)
+
+    } else {
+        success = await Caller.signOut(token)
+    }
+
+    return success
+}
+
+
 /**
  * Make sure you have configured SocialAuth's googleClientId before calling
  * this function
  * @param callback A response object returned from google or an error otherwise
  */
 export const googleSignIn = (callback: (error: string | null, response: SocialAuthResponse | null) => void) => {
-    if (Config.googleClientID === undefined || Config.googleClientID.length == 0) {
-        const error = "You must configure a client ID before using SocialAuth"
-        console.warn(error)
-        throw Error(error)
+    if (isProviderInvalid("google")) {
+        throw SocialAuthError.withCode(SocialAuthErrorCode.CLIENT_ID)
     }
 
-    Caller.googleSignIn(Config.googleClientID, createCallback("google", callback))
+    Caller.googleSignIn(Config.googleClientID!, createCallback("google", callback))
 }
 
 /**
@@ -45,20 +95,16 @@ export const googleSignIn = (callback: (error: string | null, response: SocialAu
  * @param callback A response object returned from google or an error otherwise
  */
 export const facebookSignIn = (callback: (error: string | null, response: SocialAuthResponse | null) => void) => {
-    if (Config.facebookAppID === undefined || Config.facebookAppID.length == 0) {
-        const error = "You must configure a client ID before using SocialAuth"
-        console.warn(error)
-        throw Error(error)
+    if (isProviderInvalid("facebook")) {
+        throw SocialAuthError.withCode(SocialAuthErrorCode.CLIENT_ID)
     }
 
-    Caller.facebookSignIn(Config.facebookAppID, createCallback("facebook", callback))
+    Caller.facebookSignIn(Config.facebookAppID!, createCallback("facebook", callback))
 }
 
 export const appleSignIn = (callback: (error: string | null, response: SocialAuthResponse | null) => void) => {
-    if (Platform.OS === "android") {
-        const error = "Apple Sign In only available on iOS"
-        console.error(error)
-        throw Error(error)
+    if (isProviderInvalid("apple")) {
+        console.error(SocialAuthError.withCode(SocialAuthErrorCode.IOS_ONLY))
     }
 
     Caller.appleSignIn(createCallback("apple", callback))
